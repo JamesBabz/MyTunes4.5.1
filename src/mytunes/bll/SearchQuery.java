aaee1f91ed1c7e76ec05d5dbc1a3c8f8c3f32f6f
frodzet <simon.birkedal@gmail.com>
2016-12-09 03:11:08 +0100

    Added some javadoc.

--- src/mytunes/bll/SearchQuery.java
+++ src/mytunes/bll/SearchQuery.java
@@ -7,11 +7,19 @@ import javafx.collections.ObservableList;
 import mytunes.be.Song;
 
 /**
- *
+ * 
  * @author Simon Birkedal
  */
 public class SearchQuery
-{    
+{ 
+    
+    /**
+     * Searches through a list of songs, then returns a new ArrayList of songs
+     * that contains the searchquery requirement.
+     * @param songs The list of songs to go through.
+     * @param searchQuery The string to match the songs against.
+     * @return Returns a new ArrayList of songs that matches the searchquery.
+     */
     public ArrayList<Song> search(List<Song> songs, String searchQuery)
     {        
         ArrayList<Song> result = new ArrayList<>();
